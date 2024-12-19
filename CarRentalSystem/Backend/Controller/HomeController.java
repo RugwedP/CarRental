8175fa1791a3e005491b1544df91d17f099a8db3
RugwedP <121621867+RugwedP@users.noreply.github.com>
2024-12-19 12:44:26 +0530

    Add files via upload

---
+++ CarRentalSystem/Backend/Controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.project.carrentalsystem.Controller;
+
+import  org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties.ShowSummary;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.carrentalsystem.Model.Employee;
+import com.project.carrentalsystem.Service.EmployeeService;
+
+@Controller
+public class HomeController {
+
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@GetMapping("/")
+	public String homePage(Model model)
+	{
+		return "index";
+	}
+	
+	
+	
+	@PostMapping("/registerEmployee") 
+    public String addEmployee(@ModelAttribute("employee") Employee employee) {
+        Employee savedEmployee = employeeService.addEmployee(employee);
+        System.out.println("............................."+employee.getAccountType());
+        System.out.println("saved employee ----------------"+savedEmployee);
+        return "registrationSuccess"; 
+    }
+	
+	
+	 	@GetMapping("/registerEmployee")
+	    public String showRegisterEmployeePage(Model model) {
+	        model.addAttribute("employee", new Employee());
+	        return "registerEmployee";
+	    }
+}
